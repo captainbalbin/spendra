e4f5e89a46f80470b5f422b6540fb2f850431546
captainbalbin <albin@sjostrand.dev>
2024-05-20 19:50:11 +0200

    update run script and tailwindcss config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['index.html', './src/**/*.{ts,tsx}'],
+  content: [
+    'index.html',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
