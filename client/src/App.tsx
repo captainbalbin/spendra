6b0d7511b931962a1cd20d6a66646c4593bb8ecc
captainbalbin <albin@sjostrand.dev>
2024-05-20 20:16:38 +0200

    add shadcn ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from './components/ui/button'
+
 function App() {
-  return <>Hello World!</>
+  return (
+    <div className="bg-background h-screen">
+      <p className="text-foreground">Hello World!</p>
+      <Button>Click me!</Button>
+    </div>
+  )
 }
 
 export default App
