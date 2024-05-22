e07d6b0a8edf355f4d827f36d6193683fa0d681c
captainbalbin <albin@sjostrand.dev>
2024-05-22 19:23:13 +0200

    add react-hook-form and form ui

--- client/src/containers/expenseDialog.tsx
+++ client/src/containers/expenseDialog.tsx
@@ -1,5 +1,6 @@
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogHeader,
@@ -7,29 +8,76 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog'
 import { Button } from '@/components/ui/button'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { useRef } from 'react'
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
 
 export const ExpenseDialog = () => {
+  const closeRef = useRef<HTMLButtonElement | null>(null)
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    if (form.formState.isValid) {
+      closeRef?.current?.click()
+    }
+    console.log(values)
+  }
+
   return (
     <Dialog>
-      <DialogTrigger>
+      <DialogTrigger asChild>
         <Button>New Expense</Button>
       </DialogTrigger>
-      <DialogContent>
+      <DialogContent
+        onEscapeKeyDown={(e) => (form.formState.isDirty ? e.preventDefault() : null)}
+        onInteractOutside={(e) => (form.formState.isDirty ? e.preventDefault() : null)}
+      >
         <DialogHeader>
           <DialogTitle>New Expense</DialogTitle>
           <DialogDescription>Add a new expense</DialogDescription>
         </DialogHeader>
-        <form>
-          <label>
-            Title
-            <input type="text" />
-          </label>
-          <label>
-            Amount
-            <input type="number" />
-          </label>
-          <button type="submit">Save</button>
-        </form>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" disabled={!form.formState.isValid}>
+              {form.formState.isLoading ? 'Loading...' : 'Submit'}
+            </Button>
+          </form>
+        </Form>
+        <DialogClose ref={closeRef} />
       </DialogContent>
     </Dialog>
   )
